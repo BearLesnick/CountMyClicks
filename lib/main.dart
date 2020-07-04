32041d1bbb0e9dcab4af1e41e0a556ce3249675c
elereman <123456mayday@gmail.com>
2020-07-04 20:16:59 +0300

    Added dependencies to use firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:how_do_i_do/domain/counter/service.dart';
 import 'package:how_do_i_do/home_page.dart';
 import 'package:how_do_i_do/stub/domain/counter/facade.dart';
+import 'package:how_do_i_do/stub/domain/counter/firebase_repository.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: <SingleChildWidget>[
-        Provider<CounterRepository>.value(value: StubCounterRepository()),
+        Provider<CounterRepository>.value(value: FireBaseCounterRepository()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
